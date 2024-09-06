4a435fdbd3af381cd784d562b616f85ebb6b71c4
Steve Strates <steve@strates.io>
2024-09-06 03:08:58 -0400

    Start with just utils

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,9 +1,2 @@
-// Phoenix
-export * from './phoenix/LiveChannel';
-export * from './phoenix/LiveSocket';
-export * from './phoenix/LiveViewModel';
-export * from './phoenix/PhoenixConnection';
-
-// Utils
-export * from './utils/rxjs';
-export * from './utils/strings';
+export { snakeToCamelCase, snakeToTitleCase } from './utils/strings.js';
+export { isNotNull } from './utils/rxjs.js';
