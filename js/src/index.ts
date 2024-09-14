edfc54ecc92e91c10366c30aba86207591bb5b54
Steve Strates <steve@strates.io>
2024-09-13 22:04:02 -0400

    Export all the things

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,32 +1,10 @@
-// export { PhoenixChannelError, PhoenixSocketError } from './phoenix.js';
-// export { LiveSocket } from './socket.js';
-// export { LiveChannel, LiveChannelEvent } from './channel.js';
-
-export {
-  // LiveEventStream,
-  LiveError,
-  LiveErrorType,
-  LiveEvent,
-  LiveEventType,
-} from './events.js';
-
-export { LiveConnection, connect } from './connect.js';
-
-export {
-  liveViewModel,
-  LiveViewModel,
-  join,
-  leave,
-  action,
-  computed,
-  liveError,
-  liveEvent,
-  liveObservable,
-  localObservable,
-  onJoin,
-  onLeave,
-} from './view-model.js';
+export * from './channel.js';
+export * from './connect.js';
+export * from './events.js';
+export * from './phoenix.js';
+export * from './socket.js';
+export * from './state.js';
+export * from './view-model.js';
 
 export { snakeToCamelCase, snakeToTitleCase } from './utils/strings.js';
-
 export { LogLevel, setLogLevel } from './utils/logger.js';
